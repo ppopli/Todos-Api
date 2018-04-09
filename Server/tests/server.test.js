6830a009ad41a10914d9f1f39916cc4469e154ec
Pulkit Popli <=>
2018-04-09 21:52:57 +0530

    adding GET/todo route test

--- Server/tests/server.test.js
+++ Server/tests/server.test.js
@@ -4,10 +4,24 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
+const todos = [
+  {
+    text : "Something to cook"
+  },
+  {
+    text : "Something to eat"
+  }
+];
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done())
+  .catch((err) => {
+    return err;
+  });
 });
+
 describe('POST /todos', () => {
 
   it('Should create a new todo', (done) => {
@@ -26,8 +40,8 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          expect(todos.length).toBe(3);
+          expect(todos[2].text).toBe(text);
           done();
         }).catch((e) => done(e));
       });
@@ -44,9 +58,22 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+
+  it('Should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(() => done());
   });
 });
